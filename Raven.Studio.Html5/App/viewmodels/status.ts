b0acc0d5903c559c7aa46a83199f331c620471e6
Kijana Woodard <github@wyldeye.com>
2013-09-24 17:41:52 -0500

    status menu

--- Raven.Studio.Html5/App/viewmodels/status.ts
+++ Raven.Studio.Html5/App/viewmodels/status.ts
@@ -1,10 +1,36 @@
-export class status {
+import http = require("plugins/http");
+import app = require("durandal/app");
+import sys = require("durandal/system");
+import router = require("plugins/router");
+
+
+import raven = require("common/raven");
+
+class status {
 
     displayName = "status";
-    activate() { 
-        
+    activeView = ko.observable('');
+
+    ravenDb: raven;
+
+    constructor() {
+        this.ravenDb = new raven();
+    }
+
+    activate(args) { 
+        console.log(args);
+        var view = args && args.view ? args.view : 'statistics';
+        this.activeView(view);
     }
     canDeactivate() {
         return true; 
     } 
+
+    switchView(view) {
+        console.log(view);
+        this.activeView(view);
+        router.navigate("#status?view=" + view, false);
+    }
 }
+
+export = status;
